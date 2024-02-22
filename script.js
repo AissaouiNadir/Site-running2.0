0a961976b3469f0f9aa7ea4e736cfb8dbd174cc6
AissaouiNadir <144660716+AissaouiNadir@users.noreply.github.com>
2024-02-22 00:33:42 +0000

    modif de secoure c'est normal si il y a beaucoup de choses en un commit index.html style.css script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const wrapper = document.querySelector('.wrapper');
+const signinLink = document.querySelector('.signin-link');
+const signupLink = document.querySelector('.signup-link');
+
+signupLink.addEventListener('click', () => {
+    wrapper.classList.add('active');
+});
+
+signinLink.addEventListener('click', () => {
+  wrapper.classList.remove('active');
+});
